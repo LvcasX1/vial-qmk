797ed49bc4acb94aa3ee93382419e1a71aa7cc01
Lucas Campos <ilucas.gp7@gmail.com>
2026-01-06 02:17:24 -0300

    fix

--- keyboards/crkbd/keymaps/vial/keymap.c
+++ keyboards/crkbd/keymaps/vial/keymap.c
@@ -4,67 +4,35 @@ Copyright 2020 Drashna Jaelre <@drashna>
 */
 #include QMK_KEYBOARD_H
 
-enum custom_keycodes {
-    CMD_ALT_BSPC = QK_USER_0
-};
+// Mod-Tap: Tap=Backspace, Hold=Alt+Cmd (0x6C2A)
+#define CMD_ALT_BSPC MT(MOD_LALT | MOD_LGUI, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
-                                          KC_LGUI, TL_LOWR,  KC_SPC,     KC_ENT, TL_UPPR, KC_RALT
+                                          KC_LGUI, TL_LOWR,  KC_SPC,   CMD_ALT_BSPC, TL_UPPR, KC_RALT
   ),
   [1] = LAYOUT_split_3x6_3(
        KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          KC_LGUI, _______,  KC_SPC,   CMD_ALT_BSPC, _______, KC_RALT
   ),
   [2] = LAYOUT_split_3x6_3(
        KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          KC_LGUI, _______,  KC_SPC,   CMD_ALT_BSPC, _______, KC_RALT
   ),
   [3] = LAYOUT_split_3x6_3(
       QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          KC_LGUI, _______,  KC_SPC,   CMD_ALT_BSPC, _______, KC_RALT
   )
 };
-
-// ========== CUSTOM KEYCODE (FUERA del bloque OLED) ==========
-static uint16_t cmd_alt_timer;
-
-#ifdef OLED_ENABLE
-void set_keylog(uint16_t keycode, keyrecord_t *record);  // Forward declaration
-#endif
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-#ifdef OLED_ENABLE
-    if (record->event.pressed) {
-        set_keylog(keycode, record);
-    }
-#endif
-
-    switch (keycode) {
-        case CMD_ALT_BSPC:
-            if (record->event.pressed) {
-                // cmd_alt_timer = timer_read();
-                // register_mods(MOD_LALT | MOD_LGUI);
-                tap_code(KC_A);
-            } else {
-                unregister_mods(MOD_LALT | MOD_LGUI);
-                if (timer_elapsed(cmd_alt_timer) < TAPPING_TERM) {
-                    tap_code(KC_BSPC);
-                }
-            }
-            return false;
-    }
-    return true;
-}
 
 // ========== OLED ==========
 #ifdef OLED_ENABLE
@@ -148,5 +116,12 @@ bool oled_task_user(void) {
         oled_render_logo();
     }
     return false;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        set_keylog(keycode, record);
+    }
+    return true;
 }
 #endif // OLED_ENABLE
