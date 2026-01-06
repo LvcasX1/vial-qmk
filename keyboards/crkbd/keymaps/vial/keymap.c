2475e8e4274051721b8846460b13b1ad501a2158
Lucas Campos <ilucas.gp7@gmail.com>
2026-01-06 01:06:07 -0300

    adds cmd_alt key press to the firmware

--- keyboards/crkbd/keymaps/vial/keymap.c
+++ keyboards/crkbd/keymaps/vial/keymap.c
@@ -18,6 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#define CMD_ALT_BSPC MT(MOD_LALT | MOD_LGUI, KC_BSPC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
