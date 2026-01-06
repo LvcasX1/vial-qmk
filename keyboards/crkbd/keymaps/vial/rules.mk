df188b0059dbabd09450bfd354a1992e8ae809b7
Lucas Campos <ilucas.gp7@gmail.com>
2026-01-06 01:15:08 -0300

    saves some more space

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -1,13 +1,13 @@
 VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
-# LTO_ENABLE          = yes
-
 RGBLIGHT_ENABLE     = yes
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
 OLED_ENABLE         = yes
 EXTRAKEY_ENABLE     = no
 COMBO_ENABLE        = no
+TAP_DANCE_ENABLE    = no
+KEY_OVERRIDE_ENABLE = no
 
 QMK_SETTINGS        = no
 
