aa741e6188dafa38ac9d91fb7a03697172dd42b5
Lucas Campos <ilucas.gp7@gmail.com>
2026-01-06 00:30:42 -0300

    updates tapping_term

--- keyboards/crkbd/keymaps/vial/config.h
+++ keyboards/crkbd/keymaps/vial/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #undef DYNAMIC_KEYMAP_LAYER_COUNT
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#define TAPPING_TERM 180
+#define TAPPING_TERM 200
 
 //#define USE_MATRIX_I2C
 #ifdef KEYBOARD_crkbd_rev1_legacy
