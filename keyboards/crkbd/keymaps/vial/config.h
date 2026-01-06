3630f22d8101e00bc1f79b82f6b81a1039d13973
Lucas Campos <ilucas.gp7@gmail.com>
2026-01-06 00:54:16 -0300

    adds oled screens support

--- keyboards/crkbd/keymaps/vial/config.h
+++ keyboards/crkbd/keymaps/vial/config.h
@@ -26,6 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 #define TAPPING_TERM 200
 #define QMK_KEYS_PER_SCAN 4
+#define OLED_TIMEOUT 30000
 
 //#define USE_MATRIX_I2C
 #ifdef KEYBOARD_crkbd_rev1_legacy
