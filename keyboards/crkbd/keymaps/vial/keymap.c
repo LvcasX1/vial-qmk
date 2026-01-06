1104dca6b5e8a55fa4780e7463b6abc0832038da
Lucas Campos <ilucas.gp7@gmail.com>
2026-01-06 01:35:25 -0300

    fixes vial keymap file

--- keyboards/crkbd/keymaps/vial/keymap.c
+++ keyboards/crkbd/keymaps/vial/keymap.c
@@ -24,25 +24,6 @@ enum custom_keycodes {
     CMD_ALT_BSPC = QK_USER_0
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case CMD_ALT_BSPC:
-            if (record->tap.count && record->event.pressed) {
-                // Tap: Backspace
-                tap_code(KC_BSPC);
-                return false;
-            } else if (record->event.pressed) {
-                // Hold: Alt + Cmd
-                register_mods(MOD_LALT | MOD_LGUI);
-            } else {
-                // Release
-                unregister_mods(MOD_LALT | MOD_LGUI);
-            }
-            return false;
-    }
-    return true;
-}
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -196,6 +177,21 @@ bool oled_task_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     set_keylog(keycode, record);
+  }
+  switch (keycode) {
+    case CMD_ALT_BSPC:
+        if (record->tap.count && record->event.pressed) {
+            // Tap: Backspace
+            tap_code(KC_BSPC);
+            return false;
+        } else if (record->event.pressed) {
+            // Hold: Alt + Cmd
+            register_mods(MOD_LALT | MOD_LGUI);
+        } else {
+            // Release
+            unregister_mods(MOD_LALT | MOD_LGUI);
+        }
+        return false;
   }
   return true;
 }
