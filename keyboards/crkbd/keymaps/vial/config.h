1d4c32348cf85f4fdc30f46305c1a59bebd8b40b
Lucas Campos <ilucas.gp7@gmail.com>
2026-05-21 14:50:19 -0400

    disable RGB animations to fix slave OLED bandwidth corruption

--- keyboards/crkbd/keymaps/vial/config.h
+++ keyboards/crkbd/keymaps/vial/config.h
@@ -44,7 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 #ifdef RGBLIGHT_ENABLE
 #    undef RGBLIGHT_LED_COUNT
-#    define RGBLIGHT_ANIMATIONS
+//#    define RGBLIGHT_ANIMATIONS
 #    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
 #    define RGBLIGHT_DEFAULT_HUE 0
 #    define RGBLIGHT_DEFAULT_SAT 255
