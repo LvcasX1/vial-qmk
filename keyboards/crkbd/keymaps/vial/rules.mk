a1b157b33236e50017d07f2f23206c4edfdb85ae
Lucas Campos <ilucas.gp7@gmail.com>
2026-07-03 11:16:52 -0400

    crkbd vial: disable RGB underglow, clear RGB keys from layer 3

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -1,6 +1,6 @@
 VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
-RGBLIGHT_ENABLE     = yes
+RGBLIGHT_ENABLE     = no  # disabled: RGB adjust keys froze the split; frees flash too
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
 OLED_ENABLE         = yes
